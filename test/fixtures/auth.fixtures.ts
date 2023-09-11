df2258dcc7aef7880a7e1c42aa272e25e1767999
Rodrigo Tornaciole Correa <rodrigo.tornaciole@leanonsystems.com>
2023-09-11 16:35:15 -0300

    remove console

--- test/fixtures/auth.fixtures.ts
+++ test/fixtures/auth.fixtures.ts
@@ -2,8 +2,6 @@ import { AuthLoginResponseDTO } from '../../src/auth/dto/auth.login.response.dto
 import fetch from 'node-fetch';
 
 export const authLoginFixture = async (data, url, app): Promise<AuthLoginResponseDTO> => {
-    // return await request(app.getHttpServer()).post(url).send(data)
-    console.log(`${url}`)
     const response =  await fetch(`${url}`, {method:"POST", body: JSON.stringify(data)})
     return await response.json();
     
